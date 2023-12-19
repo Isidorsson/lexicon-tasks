995cc0aad96802a95324102bee9e1d1ca0e70091
Andreas Isidorsson <isidorssona@gmail.com>
2023-12-19 10:54:04 +0100

    Add random highlighting to a day in the calendar

---
+++ lexicon-task-calendar/app.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+  var dates = document.querySelectorAll('.dates-grid span');
+  var randomIndex = Math.floor(Math.random() * dates.length);
+  dates[randomIndex].classList.add('highlighted-day');
+}
