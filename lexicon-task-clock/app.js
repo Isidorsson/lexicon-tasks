992be01d8aaa3acb0e8ea04d56ff0ecf863502ef
Andreas Isidorsson <isidorssona@gmail.com>
2023-12-21 10:09:59 +0100

    Refactor clock drawing code

--- lexicon-task-clock/app.js
+++ lexicon-task-clock/app.js
@@ -1,125 +1,109 @@
-// Get a reference to the canvas
 const canvas = document.getElementById("canvas");
-
-// Get the 2D context from the canvas
 const ctx = canvas.getContext("2d");
+let radius = canvas.height / 2;
+ctx.translate(radius, radius);
+radius = radius * 0.90
+//drawClock();
+setInterval(drawClock, 1000);
 
-// Set the center of the clock
-const centerX = canvas.width / 2;
-const centerY = canvas.height / 2;
-
-// Draw a circle for the clock face
-ctx.beginPath();
-ctx.arc(centerX, centerY, 100, 0, 2 * Math.PI);
-ctx.stroke();
-
-// Generate a random time
-const date = new Date();
-date.setHours(Math.floor(Math.random() * 24));
-date.setMinutes(Math.floor(Math.random() * 60));
-date.setSeconds(Math.floor(Math.random() * 60));
-
-// Get the hours, minutes, and seconds
-const hours = date.getHours();
-const minutes = date.getMinutes();
-const seconds = date.getSeconds();
-
-// Calculate the angles for the clock hands
-const hoursAngle =
-  (((hours % 12) + minutes / 60) * 2 * Math.PI) / 12 - Math.PI / 2;
-const minutesAngle =
-  ((minutes + seconds / 60) * 2 * Math.PI) / 60 - Math.PI / 2;
-const secondsAngle = (seconds * 2 * Math.PI) / 60 - Math.PI / 2;
-
-// Draw the hour hand
-ctx.beginPath();
-ctx.moveTo(centerX, centerY);
-ctx.lineTo(
-  centerX + 50 * Math.cos(hoursAngle),
-  centerY + 50 * Math.sin(hoursAngle)
-);
-ctx.stroke();
-
-// Draw the minute hand
-ctx.beginPath();
-ctx.moveTo(centerX, centerY);
-ctx.lineTo(
-  centerX + 75 * Math.cos(minutesAngle),
-  centerY + 75 * Math.sin(minutesAngle)
-);
-ctx.stroke();
-
-// Draw the second hand
-ctx.beginPath();
-ctx.moveTo(centerX, centerY);
-ctx.lineTo(
-  centerX + 90 * Math.cos(secondsAngle),
-  centerY + 90 * Math.sin(secondsAngle)
-);
-ctx.stroke();
-
-// Get the time in 24-hour format, Swedish locale (sv-SE)
-// const timeText = date.toLocaleTimeString('sv-SE', { hour12: false });
-const timeText = date.toLocaleTimeString("en-US", { hour12: true });
-
-// console.log(timeText);
-
-// Get a reference to the time element
-const timeElement = document.getElementById("time");
-
-// Get the numbers
-const numbers = document.querySelectorAll(".number");
-
-// Update the rotation of the numbers
-numbers.forEach((number, index) => {
-  const angle = (index * 360) / 12;
-  number.style.transform = `rotate(${angle}deg) translateY(-40px)`;
-});
-
-//Probably better way to convert this probably should used a library:)
-const numberToWords = {
-  0: "zero",
-  1: "one",
-  2: "two",
-  3: "three",
-  4: "four",
-  5: "five",
-  6: "six",
-  7: "seven",
-  8: "eight",
-  9: "nine",
-  10: "ten",
-  11: "eleven",
-  12: "twelve",
-  13: "thirteen",
-  14: "fourteen",
-  15: "fifteen",
-  16: "sixteen",
-  17: "seventeen",
-  18: "eighteen",
-  19: "nineteen",
-  20: "twenty",
-  30: "thirty",
-  40: "forty",
-  50: "fifty",
-};
-
-function timeToWords(time) {
-  const [hours, minutes] = time.split(":").map(Number);
-  return `${numberToWords[hours]} ${
-    minutes < 20
-      ? numberToWords[minutes]
-      : numberToWords[minutes - (minutes % 10)] +
-        " " +
-        numberToWords[minutes % 10]
-  }`;
+function drawClock() {
+  ctx.arc(0, 0, radius, 0 , 2 * Math.PI);
+  ctx.fillStyle = "white";
+  ctx.fill();
 }
 
-// Convert the time to words
-const timeInWords = timeToWords(timeText);
-timeElement.textContent = "It's " + timeInWords + " o'clock";
+function drawClock() {
+  drawFace(ctx, radius);
+}
 
-// use timeNum display number
-const timeNum = document.getElementById("timeNum");
-timeNum.textContent = timeText;
-// console.log(timeNum.textContent);
+function drawFace(ctx, radius) {
+  const grad = ctx.createRadialGradient(0, 0 ,radius * 0.95, 0, 0, radius * 1.05);
+  grad.addColorStop(0, '#333');
+  grad.addColorStop(0.5, 'white');
+  grad.addColorStop(1, '#333');
+
+  ctx.beginPath();
+  ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+
+  ctx.strokeStyle = grad;
+  ctx.lineWidth = radius*0.1;
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+  ctx.fillStyle = '#333';
+  ctx.fill();
+}
+
+function drawClock() {
+  drawFace(ctx, radius);
+  drawNumbers(ctx, radius);
+}
+
+function drawNumbers(ctx, radius) {
+  ctx.font = radius * 0.15 + "px arial";
+  ctx.textBaseline = "middle";
+  ctx.textAlign = "center";
+  for(let num = 1; num < 13; num++){
+    let ang = num * Math.PI / 6;
+    ctx.rotate(ang);
+    ctx.translate(0, -radius * 0.85);
+    ctx.rotate(-ang);
+    ctx.fillText(num.toString(), 0, 0);
+    ctx.rotate(ang);
+    ctx.translate(0, radius * 0.85);
+    ctx.rotate(-ang);
+  }
+}
+
+function drawClock() {
+  drawFace(ctx, radius);
+  drawNumbers(ctx, radius);
+  drawTime(ctx, radius);
+}
+
+function drawTime(ctx, radius) {
+  const now = new Date();
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let second = now.getSeconds();
+  hour = hour%12;
+  hour = (hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60));
+  drawHand(ctx, hour, radius*0.5, radius*0.07);
+  minute = (minute*Math.PI/30)+(second*Math.PI/(30*60));
+  drawHand(ctx, minute, radius*0.8, radius*0.07);
+  second = (second*Math.PI/30);
+  drawHand(ctx, second, radius*0.9, radius*0.02);
+}
+
+function drawHand(ctx, pos, length, width) {
+  ctx.beginPath();
+  ctx.lineWidth = width;
+  ctx.lineCap = "round";
+  ctx.moveTo(0,0);
+  ctx.rotate(pos);
+  ctx.lineTo(0, -length);
+  ctx.stroke();
+  ctx.rotate(-pos);
+}
+
+
+const button = document.querySelector("button");
+const timeWord = document.querySelector(".time-word");
+const timeNum = document.querySelector(".time-num");
+
+button.addEventListener("click", () => {
+  if (timeWord.style.display === "none") {
+    timeWord.style.display = "block";
+    timeNum.style.display = "none";
+  } else {
+    timeWord.style.display = "none";
+    timeNum.style.display = "block";
+  }
+}
+);
+
+
+
