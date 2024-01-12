56aa433f599f31174d3f749b689a28a553d49373
Andreas Isidorsson <isidorssona@gmail.com>
2024-01-12 11:03:55 +0100

    Add calculator functionality to index.js

--- lexicon-task-Data-entry-alert-cal/index.js
+++ lexicon-task-Data-entry-alert-cal/index.js
@@ -19,3 +19,38 @@ let result = eval(`${firstNumber} ${operation === 'add' ? '+' : operation === 's
 
 alert(`The result is ${result}`);
 console.log(`The result is ${result}`);
+
+
+// Okej, nu är det dags att programmera en ordentlig, fast enkel miniräknare i JavaScript. Nedan kommer instruktionerna. Ni behöver som alltid en index.html och en index.js som måste vara ihopkopplade.
+// 1. Skapa en prompt som frågar om ett valfritt tal. Spara undan i en variabel.
+// 2. Skapa en prompt till som frågar om ett annat tal. Spara i en variabel.
+// 3. Skapa ny en tredje prompt som frågar vilket räknesätt användare vill använda. Det bör framgå att endast +, -, \* och / är möjliga. Denna ska också sparas i en variabel.
+// 4. Skapa en if-else-if check där du checkar de olika alternativen. Till exempel. Om räknesättet är lika med addition, så ska en addition utföras mellan de två talen och en alert ska poppa upp och redovisa resultatet. Är det subtraktion som gäller så ska subtraktion ske mellan talen och så vidare.
+// 5. Inkludera en else i slutet som hanterar alla utfall, om till exempel räknesättet skrivs in fel så det inte känns igen eller liknande.
+// 6. Lägga gärna till lite skjyssta alerts innan och efter som hälsar en välkommen och säger hejdå och så.
+
+let firstNumber1 = parseFloat(prompt('Enter a number'));
+let secondNumber1 = parseFloat(prompt('Enter another number'));
+
+let operation1 = prompt('What operation1 do you want to perform? (add, sub, mul, div)');
+let result1;
+
+if (operation1 === 'add') {
+    result1 = firstNumber1 + secondNumber1;
+    alert(`The result1 is ${result1}`);
+}
+else if (operation1 === 'sub') {
+    result1 = firstNumber1 - secondNumber1;
+    alert(`The result1 is ${result1}`);
+}
+else if (operation1 === 'mul') {
+    result1 = firstNumber1 * secondNumber1;
+    alert(`The result1 is ${result1}`);
+}
+else if (operation1 === 'div') {
+    result1 = firstNumber1 / secondNumber1;
+    alert(`The result1 is ${result1}`);
+}
+else {
+    alert('Invalid operation');
+}
