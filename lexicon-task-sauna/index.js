719985d450df41224e9baaff877fd4891c0be02b
Andreas Isidorsson <isidorssona@gmail.com>
2024-01-16 16:04:01 +0100

    Sauna file

---
+++ lexicon-task-sauna/index.js
@@ -0,0 +1,3 @@
+//Just run this inside of the console in the browser to see it work. Will prompt you to input a temperature in Fahrenheit like 165 ish and then it will convert it to Celsius and tell you if it's too hot or too cold or just right. If it's too hot or too cold it will prompt you to enter a new temperature in Fahrenheit. If it's just right it will tell you it's perfect and stop the loop.
+
+
