434583d165c6a60f8e5606f43acc8e4e9c54f0d6
Andreas Isidorsson <isidorssona@gmail.com>
2024-01-22 13:09:28 +0100

    Add and display randomText in index.js

--- lexicon-task-bucket-list/index.js
+++ lexicon-task-bucket-list/index.js
@@ -1,33 +1,23 @@
+// const p = document.createElement("p");
+// p.innerText = "learn go";
+// document.body.appendChild(p);
 
-const p = document.createElement("p");
-p.innerText = "learn go";
-document.body.appendChild(p);
-
-const p2 = document.createElement("p");
-p2.innerText = "learn svelte";
-document.body.insertAdjacentElement("beforeend", p2);
-
-const list = document.querySelector(".list");
-console.log(list.innerHTML);
-
-list.innerHTML = "<div>This is a div element as a string</div>";
+// const p2 = document.createElement("p");
+// p2.innerText = "learn svelte";
+// document.body.insertAdjacentElement("beforeend", p2);
 
 
+// const list = document.querySelector(".list");
+// console.log(list.innerHTML);
 
 
+// list.innerHTML = "<div>This is a div element as a string</div>";
 
 
-// const randomText = [
-//   "Skydiving",
-//   "Traveling to all seven continents",
-//   "Learning a new language",
-//   "Writing a book",
-//   "Running a marathon",
-//   "Scuba diving",
-//   "Starting a business",
-//   "Learning to play a musical instrument",
-//   "Volunteering abroad",
-//   "Bungee jumping"
-// ];
-
+const randomText =[ "eaque", "autem", "quis", "in", "nemo" ]
+for (let i = 0; i < randomText.length; i++) {
+  const li = document.createElement("p");
+  li.innerText = randomText[i];
+  document.body.appendChild(li);
+}
 
