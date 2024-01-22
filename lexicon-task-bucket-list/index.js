8393da427ce2b0aef50744564dfa1a6d9675bf85
Andreas Isidorsson <isidorssona@gmail.com>
2024-01-22 12:45:40 +0100

    Remove index.js from .gitignore

---
+++ lexicon-task-bucket-list/index.js
@@ -0,0 +1,37 @@
+
+
+
+const p = document.createElement("p");
+p.innerText = "travel";
+document.body.appendChild(p);
+
+
+const p2 = document.createElement("p");
+p2.innerText = "diving";
+// document.body.insertAdjacentElement("beforeend", p2);
+document.body.insertAdjacentElement("beforeend", p2);
+
+
+
+const list = document.querySelector(".list");
+console.log(list.innerHTML);
+
+
+
+
+
+
+// const randomText = [
+//   "Skydiving",
+//   "Traveling to all seven continents",
+//   "Learning a new language",
+//   "Writing a book",
+//   "Running a marathon",
+//   "Scuba diving",
+//   "Starting a business",
+//   "Learning to play a musical instrument",
+//   "Volunteering abroad",
+//   "Bungee jumping"
+// ];
+
+
