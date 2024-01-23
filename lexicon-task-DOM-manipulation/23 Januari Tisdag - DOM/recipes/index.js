37955423c766d8d0f7991ae5cddeecef37241bdc
Andreas Isidorsson <isidorssona@gmail.com>
2024-01-23 09:35:20 +0100

    Add recipe details logging

--- lexicon-task-DOM-manipulation/23 Januari Tisdag - DOM/recipes/index.js
+++ lexicon-task-DOM-manipulation/23 Januari Tisdag - DOM/recipes/index.js
@@ -1,2 +1,14 @@
-const recipeName = document.getElementById('recipe-name').innerText;
+// 1. What is the name of the recipe?
+const recipeNameElement = document.getElementById('recipe-name');
+const recipeName = recipeNameElement.innerText;
+
 console.log(`Recipe Name: ${recipeName}`);
+
+// 2. What HTML tag is used to display the Recipe name?
+const recipeNameTag = recipeNameElement.tagName;
+console.log(`Recipe Name Tag: ${recipeNameTag}`);
+
+// 3. What is the font size of the paragraph tag with the class _"description"_.
+const descriptionElement = document.querySelector('.description');
+const descriptionFontSize = window.getComputedStyle(descriptionElement).fontSize;
+console.log(`Description Font Size: ${descriptionFontSize}`);
