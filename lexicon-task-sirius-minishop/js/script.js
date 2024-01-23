ddf076d7e82f5c43cd7c987303214131ab654040
Andreas Isidorsson <isidorssona@gmail.com>
2024-01-23 13:09:15 +0100

    Update site address and change color of p tags

--- lexicon-task-sirius-minishop/js/script.js
+++ lexicon-task-sirius-minishop/js/script.js
@@ -25,3 +25,11 @@ document.querySelector('.art-3 button').textContent = 'Ok';
 //6. change the background of a product
 document.querySelector('.art-1').style.backgroundColor = 'lightblue';
 
+//7. change adress on the site
+document.querySelector('footer p').innerHTML = 'Potato Street 1,<br>123 45<br>Potato City';
+
+//8. change color of the p tags
+document.querySelectorAll('p').forEach(p => {
+  p.style.color = 'green';
+});
+
