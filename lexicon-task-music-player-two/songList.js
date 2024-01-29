c73bf65c76f2c24a40eb1964c9741497a96fe854
Andreas Isidorsson <isidorssona@gmail.com>
2024-01-29 13:01:11 +0100

    Add music files and update song list

---
+++ lexicon-task-music-player-two/songList.js
@@ -0,0 +1,74 @@
+export const songsList = [
+  {
+    "title": "Immortal ft. MARINA",
+    "artist": "MewOne! & Syberian",
+    "album": "Dont know",
+    "link": "MARINA - Immortal (CodeMiko Intro) MewOne! Syberian Beast Remix [TubeRipper.com].m4a",
+    "thumb": "https://i.ytimg.com/vi/Z0PqfNHiwO8/maxresdefault.jpg"
+  },
+
+  {
+    "title": "Little Payo PPC - Ykanic ft. Payo",
+    "artist": "Ykanic SoundCloud",
+    "album": "Ykanic 3 years",
+    "link": "Y2meta.app - Little Payo PPC - Ykanic (128 kbps).mp3",
+    "thumb": "https://i3.ytimg.com/vi/gsprhoGhz3A/hqdefault.jpg"
+  },
+  {
+    "title": "THAT'S WHAT'S UP ft. Mitch Jones",
+    "artist": "Mitch Jones",
+    "album": "WANDAI 3 years",
+    "link": "THAT'S WHAT'S UP ft. WANDAI (Mitch Jones Music Video).mp3",
+    "thumb": "https://i3.ytimg.com/vi/-Cxgbqp1N4M/maxresdefault.jpg"
+  },
+  {
+    "title": "SUP YALL ft. ASMONGOLD",
+    "artist": "Constera & ASMONGOLD",
+    "album": "Constera 5 years",
+    "link": "x2mate.com - 🎵 SUP YALL (Asmongold Music Video) 🎵 (128 kbps).mp3",
+    "thumb": "https://img.youtube.com/vi/DaJ5tQWPS9A/sddefault.jpg"
+  },
+  {
+    "title": "SODA YOU'RE SO ft. SODAPOPPIN",
+    "artist": "Constera & Sodapoppin",
+    "album": "Constera 3 years",
+    "link": "x2mate.com - 🎵  SODA YOU'RE SO (Sodapoppin Music Video) 🎵 (128 kbps).mp3",
+    "thumb": "https://i3.ytimg.com/vi/Hj2lIBILMNg/maxresdefault.jpg"
+  },
+  {
+    "title": "VERY GOOD ft. Esfand",
+    "artist": "Constera & Esfand",
+    "album": "Constera 3 years",
+    "link": "x2mate.com - 🎵 VERY GOOD 🎵 (Esfand Music Video) (128 kbps).mp3",
+    "thumb": "https://i3.ytimg.com/vi/cLy_-iMXjGM/maxresdefault.jpg"
+  },
+  {
+    "title": "Little Moscow ft. RudeNoodleNPC",
+    "artist": "RudeNoodle",
+    "album": "RudeNoodle 1 year",
+    "link": "x2mate.com-RudeNoodleNPC - Little Moscow-(480p).mp4",
+    "thumb": "https://i3.ytimg.com/vi/KwTVvu3dcsg/maxresdefault.jpg"
+  },
+  {
+    "title": "FAFO ft. RudeNoodleNPC",
+    "artist": "RudeNoodle",
+    "album": "RudeNoodle 1 year",
+    "link": "x2mate.com - FAFO (128 kbps).mp3",
+    "thumb": "https://i3.ytimg.com/vi/N_Blfb1Mckg/maxresdefault.jpg"
+  },
+  {
+    "title": "BBB ft. RudeNoodleNPC",
+    "artist": "RudeNoodle",
+    "album": "RudeNoodle 1 year",
+    "link": "x2mate.com - BBB (128 kbps).mp3",
+    "thumb": "https://i3.ytimg.com/vi/R8BDL5KN9tA/maxresdefault.jpg"
+  },
+  {
+    "title": "Visitation ft. RudeNoodleNPC",
+    "artist": "RudeNoodle",
+    "album": "RudeNoodle 1 year",
+    "link": "x2mate.com - Visitation (128 kbps).mp3",
+    "thumb": "https://i3.ytimg.com/vi/BsojGziPSds/maxresdefault.jpg"
+  },
+
+];
