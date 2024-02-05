9d86f8bb813fe02bd849e4eab5edccd2f0f8e77c
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-05 17:30:31 +0100

    Update index.html, index.js, and style.css

--- lexicon-task-api-bootcamp/index.js
+++ lexicon-task-api-bootcamp/index.js
@@ -29,22 +29,64 @@ fetch("https://majazocom.github.io/Data/dogs.json")
 
       parentDiv.appendChild(dogDiv);
     });
-  });
+  })
+  .catch((error) => console.error("Error:", error));
 
 fetch("https://majazocom.github.io/Data/books.json")
   .then((response) => response.json())
   .then((data) => {
     const parentDiv = document.querySelector(".display-book");
     data.forEach((book) => {
-      console.log(book);
+      // console.log(book);
       const bookDiv = document.createElement("grid-book");
       if (book.pages < 500) {
-        bookDiv.innerHTML = `<h2>Books with less then 500 pages</h2>`;
-        bookDiv.innerHTML = `<h3>Book title: ${book.title}</h3>`;
+        bookDiv.innerHTML = `<h2>Books with less than 500 pages</h2>`;
+        bookDiv.innerHTML += `<h3>Book title: ${book.title}</h3>`;
         bookDiv.innerHTML += `<p>Author: ${book.author}</p>`;
         bookDiv.innerHTML += `<p>Pages: ${book.pages}</p>`;
         bookDiv.innerHTML += `<p>Genre: ${book.genre}</p>`;
         parentDiv.appendChild(bookDiv);
       }
     });
-  });
+  })
+  .catch((error) => console.error("Error:", error));
+
+//   name: 'Veronica', age: 23, allergies: Array(2), attending: true, image: 'https://images.unsplash.com/photo-1664575602554-20…tYW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'}
+// age
+// :
+// 23
+// allergies
+// :
+// (2) ['vegan', 'pineapple']
+// attending
+// :
+// true
+// image
+// :
+// "https://images.unsplash.com/photo-1664575602554-2087b04935a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8d29tYW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+// name
+// :
+// "Veronica"
+
+fetch("https://majazocom.github.io/Data/attendees.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const parentDiv = document.querySelector(".display-attendees");
+    data.forEach((attendee) => {
+      console.log(attendee);
+      const attendeeDiv = document.createElement("grid-attendee");
+      if (attendee.attending === true) {
+        attendeeDiv.innerHTML = `<h2>Attendees</h2>`;
+        attendeeDiv.innerHTML += `<h3>Name: ${attendee.name}</h3>`;
+        attendeeDiv.innerHTML += `<p>Age: ${attendee.age}</p>`;
+        attendeeDiv.innerHTML += `<p>Allergies: ${attendee.allergies}</p>`;
+        attendeeDiv.innerHTML += `<p>Attending: ${attendee.attending}</p>`;
+        attendeeDiv.innerHTML += `<img src="${attendee.image}">`;
+        
+        parentDiv.appendChild(attendeeDiv);
+      }
+    });
+  })
+  .catch((error) => console.error("Error:", error));
+ 
+  
