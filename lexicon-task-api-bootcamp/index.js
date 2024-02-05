dffaae73a8fdc1c5294afca0ddf3ece525e99b20
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-05 16:43:04 +0100

    Update index.html and index.js files to display books information

--- lexicon-task-api-bootcamp/index.js
+++ lexicon-task-api-bootcamp/index.js
@@ -14,7 +14,7 @@ fetch("https://majazocom.github.io/Data/pokemons.json")
 fetch("https://majazocom.github.io/Data/dogs.json")
   .then((response) => response.json())
   .then((data) => {
-    const parentDiv = document.querySelector(".dog-div");
+    const parentDiv = document.querySelector(".display-dog");
     data.forEach((dog) => {
       const dogDiv = document.createElement("grid-dog");
       dogDiv.innerHTML = `<h3>Dog name: ${dog.name}</h3>`;
@@ -28,5 +28,25 @@ fetch("https://majazocom.github.io/Data/dogs.json")
       dogDiv.innerHTML += `<p>Present: ${dog.present}</p>`;
 
       parentDiv.appendChild(dogDiv);
+    });
+  });
+
+fetch("https://majazocom.github.io/Data/books.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const parentDiv = document.querySelector(".display-book");
+    data.forEach((book) => {
+      console.log(book);
+      const bookDiv = document.createElement("grid-book");
+      // render if less then 500 pages
+      if (book.pages < 500) {
+        //  add text saying all books with less then 500 pages
+        bookDiv.innerHTML = `<h2>Books with less then 500 pages</h2>`;
+        bookDiv.innerHTML = `<h3>Book title: ${book.title}</h3>`;
+        bookDiv.innerHTML += `<p>Author: ${book.author}</p>`;
+        bookDiv.innerHTML += `<p>Pages: ${book.pages}</p>`;
+        bookDiv.innerHTML += `<p>Genre: ${book.genre}</p>`;
+        parentDiv.appendChild(bookDiv);
+      }
     });
   });
