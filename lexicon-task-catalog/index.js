b03e98f5462aa2c121c5542eac0dc4a262300628
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-06 14:53:00 +0100

    Add loader functionality and improve code structure

--- lexicon-task-catalog/index.js
+++ lexicon-task-catalog/index.js
@@ -14,98 +14,128 @@ const pageNumber = document.querySelector(".page-number");
 const nextButton = document.querySelector(".next");
 const lastButton = document.querySelector(".last");
 
+/**
+ * The above code defines two functions, `showLoaders()` and `hideLoaders()`, which respectively
+ * display and hide elements with the class name "loader".
+ */
+function showLoaders() {
+  const loaders = document.querySelectorAll(".loader");
+  loaders.forEach((loader) => {
+    loader.style.display = "block";
+  });
+  const blur = document.querySelectorAll(".blur");
+  blur.forEach((blur) => {
+    blur.style.display = "block";
+  });
+}
+
+function hideLoaders() {
+  const loaders = document.querySelectorAll(".loader");
+  loaders.forEach((loader) => {
+    loader.style.display = "none";
+  });
+  const blur = document.querySelectorAll(".blur");
+  blur.forEach((blur) => {
+    blur.style.display = "none";
+  });
+}
+
 // Function to update character details
 function updateCharacterDetails(character) {
-  characterName.textContent = character.name;
-  characterDescription.textContent = `Height: ${character.height} Mass: ${character.mass} Hair color: ${character.hair_color} Skin color: ${character.skin_color} Eye color: ${character.eye_color} Birth year: ${character.birth_year} Gender: ${character.gender}`;
+  showLoaders();
+  setTimeout(() => {
+    characterName.textContent = character.name;
+    characterDescription.textContent = `Height: ${character.height} Mass: ${character.mass} Hair color: ${character.hair_color} Skin color: ${character.skin_color} Eye color: ${character.eye_color} Birth year: ${character.birth_year} Gender: ${character.gender}`;
+    hideLoaders();
+  }, 500);
 }
 
 // Function to update planet details
 function updatePlanetDetails(planet) {
-  planetName.textContent = planet.name;
-  planetDescription.textContent = `Rotation period: ${planet.rotation_period} Orbital period: ${planet.orbital_period} Diameter: ${planet.diameter} Climate: ${planet.climate} Gravity: ${planet.gravity} Terrain: ${planet.terrain}`;
+  showLoaders();
+  setTimeout(() => {
+    planetName.textContent = planet.name;
+    planetDescription.textContent = `Rotation period: ${planet.rotation_period} Orbital period: ${planet.orbital_period} Diameter: ${planet.diameter} Climate: ${planet.climate} Gravity: ${planet.gravity} Terrain: ${planet.terrain}`;
+    hideLoaders();
+  }, 500);
 }
 // Function to update character list
 function updateCharacterList(characters) {
   const list = document.querySelector(".list");
-  list.innerHTML = ""; 
+  list.innerHTML = "";
+
+  showLoaders();
 
   const start = (currentPage - 1) * itemsPerPage;
   const end = start + itemsPerPage;
 
   const charactersOnPage = characters.slice(start, end);
+  setTimeout(() => {
+    charactersOnPage.forEach((character) => {
+      const listItem = document.createElement("li");
+      listItem.classList.add("list-item");
 
-  charactersOnPage.forEach((character) => {
-    const listItem = document.createElement("li");
-    listItem.classList.add("list-item");
+      const characterName = document.createElement("p");
+      characterName.classList.add("character-name");
+      characterName.textContent = character.name;
 
-    const characterName = document.createElement("p");
-    characterName.classList.add("character-name");
-    characterName.textContent = character.name;
+      listItem.appendChild(characterName);
+      list.appendChild(listItem);
+    });
+    hideLoaders();
+  }, 500);
 
-    listItem.appendChild(characterName);
-    list.appendChild(listItem);
-  });
-
-  // characters.forEach((character) => {
-  //   const listItem = document.createElement("li");
-  //   listItem.classList.add("list-item");
-
-  //   const characterName = document.createElement("p");
-  //   characterName.classList.add("character-name");
-  //   characterName.textContent = character.name;
-
-  //   listItem.appendChild(characterName);
-  //   list.appendChild(listItem);
-  // });
-
-    // Update page number
-    pageNumber.textContent = currentPage;
-// Enable or disable pagination buttons
-firstButton.disabled = currentPage === 1;
-previousButton.disabled = currentPage === 1;
-nextButton.disabled = currentPage === Math.ceil(characters.length / itemsPerPage);
-lastButton.disabled = currentPage === Math.ceil(characters.length / itemsPerPage);
+  // Update page number
+  pageNumber.textContent = currentPage;
+  // Enable or disable pagination buttons
+  firstButton.disabled = currentPage === 1;
+  previousButton.disabled = currentPage === 1;
+  nextButton.disabled =
+    currentPage === Math.ceil(characters.length / itemsPerPage);
+  lastButton.disabled =
+    currentPage === Math.ceil(characters.length / itemsPerPage);
 }
-
 // Check if characters array is empty
 if (characters.length === 0) {
-  // Fetch the data from the SWAPI
-  fetch("https://swapi.dev/api/people/")
-    .then((response) => response.json())
-    .then((data) => {
-      characters = data.results;
-      console.log(characters);
+  showLoaders();
+  setTimeout(() => {
+    // Fetch the data from the SWAPI
+    fetch("https://swapi.dev/api/people/")
+      .then((response) => response.json())
+      .then((data) => {
+        characters = data.results;
 
-      // Display characters in the list
-      updateCharacterList(characters);
+        // Display characters in the list
+        updateCharacterList(characters);
 
-      // Display first character details by default
-      updateCharacterDetails(characters[0]);
+        // Display first character details by default
+        updateCharacterDetails(characters[0]);
 
-      // Fetch and display planet details for the first character
-      fetch(characters[0].homeworld)
-        .then((response) => response.json())
-        .then((planet) => updatePlanetDetails(planet));
-    });
+        // Fetch and display planet details for the first character
+        fetch(characters[0].homeworld)
+          .then((response) => response.json())
+          .then((planet) => {
+            updatePlanetDetails(planet);
+          });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    hideLoaders();
+  }, 100);
 }
-
 // Add an event listener for the 'input' event
 searchInput.addEventListener("input", function (event) {
-  // Get the search query
   const query = event.target.value.toLowerCase();
   console.log(query);
 
-  // Filter the characters array
   const filteredCharacters = characters.filter((character) => {
     return character.name.toLowerCase().includes(query);
   });
   console.log(filteredCharacters);
 
-  // Update character list with the filtered characters
   updateCharacterList(filteredCharacters);
 
-  // Update character details with the first filtered character
   if (filteredCharacters.length > 0) {
     updateCharacterDetails(filteredCharacters[0]);
 
@@ -114,8 +144,8 @@ searchInput.addEventListener("input", function (event) {
       .then((response) => response.json())
       .then((planet) => updatePlanetDetails(planet));
   }
+  currentPage = 1;
 });
-
 
 /* The code block is adding event listeners to the buttons on the page. */
 firstButton.addEventListener("click", function () {
