18b592ffac52ae5e2f27bbc215924f561836eb82
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-19 15:47:32 +0100

    Refactor navbar so it scrolls

--- lexicon-task-react-caffe-retro/src/components/Nav.jsx
+++ lexicon-task-react-caffe-retro/src/components/Nav.jsx
@@ -1,27 +1,25 @@
 import "./Nav.css";
 
 export function Nav() {
-  const link = [
-    { name: "Hot", url: "#menu-hot" },
-    { name: "Juice", url: "#menu-juicy" },
-    { name: "Cozy", url: "#menu-cozy" },
-  ]
+  const links = [
+    { name: "Hot", id: "menu-hot" },
+    { name: "Juice", id: "menu-juicy" },
+    { name: "Cozy", id: "menu-cozy" },
+  ];
 
-
+  const scrollToSection = (id) => {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+  };
 
   return (
     <nav className="navbar">
       <ul>
-        {link.map((item, index) => {
-          return (
-            <li key={index}> <a href={item
-              .url}>{item.name}</a></li>
-          )
-        }
-        )}
+        {links.map((link, index) => (
+          <li key={index}>
+            <a onClick={() => scrollToSection(link.id)}>{link.name}</a>
+          </li>
+        ))}
       </ul>
-
     </nav>
   );
 }
-
