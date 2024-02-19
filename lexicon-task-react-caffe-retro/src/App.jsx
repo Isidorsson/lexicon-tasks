ac43333295e0002d348547de74390efcdfaf58fa
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-19 14:31:56 +0100

    Update components in App.jsx

--- lexicon-task-react-caffe-retro/src/App.jsx
+++ lexicon-task-react-caffe-retro/src/App.jsx
@@ -1,15 +1,15 @@
 import { Cozy } from './components/Cozy';
 import { Hot } from './components/Hot';
-import { Juice } from './components/Juice';
+import { Juicy } from './components/juicy';
 import { Nav } from './components/Nav';
 
 export function App() {
   return (
     <>
       <Nav />
-      <Cozy />
       <Hot />
-      <Juice />
+      <Cozy />
+      <Juicy />
     </>
   );
 }
