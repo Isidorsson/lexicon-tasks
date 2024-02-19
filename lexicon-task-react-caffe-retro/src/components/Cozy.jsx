c75b25755554233af80dfc7dedbab9e9b497ea74
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-19 14:32:01 +0100

    Add cozy drinks menu to Cozy component

--- lexicon-task-react-caffe-retro/src/components/Cozy.jsx
+++ lexicon-task-react-caffe-retro/src/components/Cozy.jsx
@@ -3,7 +3,16 @@ import "./Cozy.css";
 export function Cozy() {
   return (
 
-    <h1>Cozy</h1>
+    <section id="menu-cozy">
+      <h1>Cozy</h1>
+      <p>Our cozy drinks are perfect for a cold day.</p>
+      <p>Choose from a variety of warm drinks to keep you warm and cozy.</p>
+      <span>Hot Chocolate €6.50</span>
+      <span>Chai Latte €5.00</span>
+      <span>Matcha Latte €4.50</span>
+      <span>Spiced Cider €5.50</span>
+    </section>
+    
 
   );
 }
