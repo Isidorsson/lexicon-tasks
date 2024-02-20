7af38dc05c79a7bdcb57a767c5b17030493c1d8f
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-20 11:40:43 +0100

    Add Volume component to the music player

---
+++ lexicon-task-react-music-player/src/components/Volume.tsx
@@ -0,0 +1,17 @@
+import './Volume.css';
+
+export function Volume() {
+
+
+  return (
+    <section className="volume-wrapper">
+      <i className="fas fa-volume-off" style={{ textAlign: 'right' }} onClick={() => adjustVolume(0)}></i>
+      <div className="volume-bar">
+        <input type="range" name="volume-slider" id="volume-slider" min="0" max="1" step="0.1" value="0.5"
+          onChange={(event) => adjustVolume(Number(event.target.value))} />
+        <div className="volume-trail"></div>
+      </div>
+      <i className="fas fa-volume-up" onClick={() => adjustVolume(1)}></i>
+    </section>
+  );
+}
