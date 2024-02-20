4423cd1799ab94dfdf01b74eb7f80945f9bc8edf
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-20 16:22:13 +0100

    Fix the code to hide the song for 10sec

--- lexicon-task-react-music-player/src/components/SongList.tsx
+++ lexicon-task-react-music-player/src/components/SongList.tsx
@@ -1,5 +1,7 @@
 import './SongList.css';
 
+import { useState } from 'react';
+
 /* The `interface ISong` is defining a TypeScript interface that specifies the structure of an object
 representing a song. In this case, it defines that a song object should have a property `title` of
 type `string`. This interface helps to enforce type checking and ensure that any object labeled as a
@@ -15,22 +17,34 @@ This structure helps to enforce type checking and ensure that the component rece
 data structure as props. */
 interface ISongListProps {
   SongData: { songs: ISong[] };
-  removeSong: (index: number) => void; 
 }
 
 export function SongList({ SongData }: ISongListProps) {
-  const { songs } = SongData;
-  function removeSong(index: number): void {
-    songs.splice(index, 1);
-  }
+  // const { songs } = SongData;
+  // function removeSong(index: number): void {
+  //   songs.splice(index, 1);
+  // }
+
+  const [hiddenSongs, setHiddenSongs] = useState<number[]>([]); 
+
+  const hideSong = (index: number) => {
+    setHiddenSongs(prevHiddenSongs => [...prevHiddenSongs, index]);
+  
+    setTimeout(() => {
+      setHiddenSongs(prevHiddenSongs => prevHiddenSongs.filter(i => i !== index));
+    }, 10000); // 10 seconds
+  };
 
   return (
     <aside className="song-list-wrapper">
       <ul className="song-list">
         {SongData.songs.map((song, index) => (
-          <li key={index}>{song.title}
-            <button className="btn-remove"  onClick={() => removeSong(index)}>Remove</button>
-          </li>
+          !hiddenSongs.includes(index) && (
+            <li key={index}>
+              {song.title}
+              <button className='btn-remove' onClick={() => hideSong(index)}>Remove</button>
+            </li>
+          )
         ))}
       </ul>
     </aside >
