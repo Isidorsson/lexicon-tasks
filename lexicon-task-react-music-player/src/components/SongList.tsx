08c43f88d36c5e985195f4209af3ef7952e05330
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-20 10:47:49 +0100

    Add song list to SongList component

--- lexicon-task-react-music-player/src/components/SongList.tsx
+++ lexicon-task-react-music-player/src/components/SongList.tsx
@@ -1,9 +1,15 @@
 import './SongList.css';
 
+import SongData from '../data/SongData.json';
+
 export function SongList() {
   return (
     <aside className="song-list-wrapper">
-      <ul className="song-list"></ul>
-    </aside>
+      <ul className="song-list">
+        {SongData.songs.map((song) => (
+          <li>{song.title}</li>
+        ))}
+      </ul>
+    </aside >
   );
 }
