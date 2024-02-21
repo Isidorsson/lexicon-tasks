99854f8e362b1c63b069af548bfab3f8dd8776ae
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-21 14:55:38 +0100

    Add edit functionality to TodoItem component

--- lexicon-task-react-todo-list/src/components/TodoItem.tsx
+++ lexicon-task-react-todo-list/src/components/TodoItem.tsx
@@ -6,18 +6,32 @@ interface ITodoItemProps {
   todo: ITodo;
   onToggleTodo: (id: number) => void;
   onRemoveTodo: (id: number) => void;
+  onStartEditTodo: (id: number) => void;
+  onEndEditTodo: (id: number, newText: string) => void;
+
 
 }
 
-export function TodoItem({ todo, onToggleTodo, onRemoveTodo }: ITodoItemProps) {
-  const { id, text, completed } = todo;
+export function TodoItem({ todo, onToggleTodo, onRemoveTodo, onStartEditTodo, onEndEditTodo }: ITodoItemProps) {
+  const { id, text, completed, isEditing } = todo;
+
+  const handleDoubleClick = () => {
+    onStartEditTodo(id);
+  };
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    onEndEditTodo(id, e.target.value);
+  };
 
   return (
     <li className={`todo-item ${completed ? 'completed' : ''}`}>
-      <input className='todo-checkbox' type="checkbox" checked={completed} onChange={() => onToggleTodo(id)} />
-      <span>{text}</span>
-      <button className='todo-btn-remove' onClick={() => onRemoveTodo(todo.id)}>Remove</button>
-    </li>
+    <input className='todo-checkbox' type="checkbox" checked={completed} onChange={() => onToggleTodo(id)} />
+    {isEditing ? (
+      <input type="text" defaultValue={text} onBlur={handleBlur} autoFocus />
+    ) : (
+      <span onDoubleClick={handleDoubleClick}>{text}</span>
+    )}
+    <button className='todo-btn-remove' onClick={() => onRemoveTodo(todo.id)}>Remove</button>
+  </li>
   );
 }
-
