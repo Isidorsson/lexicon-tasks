f579c01754caaf96f8e961a510a794ecc3af501b
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-22 11:43:48 +0100

    Add styles and class names to SortSelect component

--- lexicon-task-react-todo-list/src/components/SortSelect.tsx
+++ lexicon-task-react-todo-list/src/components/SortSelect.tsx
@@ -1,10 +1,12 @@
+import '../styles/SortSelect.css';
+
 import { ISortSelectProps } from "./TodoInterface";
 
 export const SortSelect: React.FC<ISortSelectProps> = ({ sortItem, setSortItem }) => {
   return (
     <div>
-      <label>Sort by:</label>
-      <select value={sortItem} onChange={event => setSortItem(event.target.value as 'asc' | 'desc' | 'completed' | 'uncompleted' | 'timestamp' | 'author')}>
+      <label className="sort-text" >Sort by:</label>
+      <select className='sort-select' value={sortItem} onChange={event => setSortItem(event.target.value as 'asc' | 'desc' | 'completed' | 'uncompleted' | 'timestamp' | 'author')}>
         <option value="timestamp">Timestamp</option>
         <option value="asc">A-Z</option>
         <option value="desc">Z-A</option>
