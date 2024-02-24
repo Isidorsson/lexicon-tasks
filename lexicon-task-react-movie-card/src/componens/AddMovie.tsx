a279229fbf8b0d56b8e951981c882649685cf331
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-24 18:13:55 +0100

    Update AddMovie component to include title input field

--- lexicon-task-react-movie-card/src/componens/AddMovie.tsx
+++ lexicon-task-react-movie-card/src/componens/AddMovie.tsx
@@ -1,13 +1,22 @@
 
 export interface IMovie { 
-
+  title: string;
+  rating: number;
+  genre: string;
+  description: string;
 }
 
 
 export const AddMovie: React.FC = () => {
   return (
     <div>
-      <h1>Add Movie</h1>
+<h2>Add Movie</h2>
+<input type="text"
+placeholder="Title"
+value={title}
+onChange={(event) => setTitle(event.target.value)}
+/>
+
     </div>
   );
 }
