22d26482c34a13eb7ba19ef7e81de841899e6767
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-24 18:14:36 +0100

    Add useState hook and input field for movie title

--- lexicon-task-react-movie-card/src/componens/AddMovie.tsx
+++ lexicon-task-react-movie-card/src/componens/AddMovie.tsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 
-export interface IMovie { 
+export interface IMovie {
   title: string;
   rating: number;
   genre: string;
@@ -8,14 +9,15 @@ export interface IMovie {
 
 
 export const AddMovie: React.FC = () => {
+  const [title, setTitle] = useState('');
   return (
     <div>
-<h2>Add Movie</h2>
-<input type="text"
-placeholder="Title"
-value={title}
-onChange={(event) => setTitle(event.target.value)}
-/>
+      <h2>Add Movie</h2>
+      <input type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
 
     </div>
   );
