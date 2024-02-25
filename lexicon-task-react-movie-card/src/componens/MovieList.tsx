921078622b66dc17704b8ec9fcf5a22f5fe6035b
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-25 16:10:15 +0100

    Add console log to display movies

--- lexicon-task-react-movie-card/src/componens/MovieList.tsx
+++ lexicon-task-react-movie-card/src/componens/MovieList.tsx
@@ -1,6 +1,10 @@
 import { IMovie } from '../componens/AddMovie';
 
 export const MovieList: React.FC<{ movies: IMovie[] }> = ({ movies }) => { 
+   
+// console.log(`Movies: ${JSON.stringify(movies)}`);
+
+  
   return (
     <div className='movie-list'>
       <h2>Movies</h2>
