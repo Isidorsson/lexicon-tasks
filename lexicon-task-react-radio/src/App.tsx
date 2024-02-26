b6eb136daac068cb36fc57d63bf56e16c3b26a61
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-26 15:28:48 +0100

    Add routes to App component

--- lexicon-task-react-radio/src/App.tsx
+++ lexicon-task-react-radio/src/App.tsx
@@ -1,11 +1,26 @@
-import {Header} from "./components/LissenDirectly";
-import {Navbar} from "./components/Navbar";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import { Header } from "./components/LissenDirectly";
+import { Navbar } from "./components/Navbar";
 
 export function App() {
-  return <>
-  
-    <Navbar />
-    <Header />
-  
-  </>;
+  return (
+    <Router>
+      <Navbar />
+      <Header />
+      <Routes>
+        <Route path="/somepath">
+        </Route>
+        <Route path="/Channel">
+        </Route>
+        <Route path="/Program">
+        </Route>
+        <Route path="/MyPage">
+        </Route>
+        <Route path="/Login">
+        </Route>
+
+      </Routes>
+    </Router>
+  );
 }
