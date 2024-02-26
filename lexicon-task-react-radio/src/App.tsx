76b66b96748d0b66301767f9f82c45725d23d8fe
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-26 15:23:12 +0100

    Add Header component and styles

--- lexicon-task-react-radio/src/App.tsx
+++ lexicon-task-react-radio/src/App.tsx
@@ -1,9 +1,11 @@
+import {Header} from "./components/LissenDirectly";
 import {Navbar} from "./components/Navbar";
 
 export function App() {
   return <>
   
     <Navbar />
+    <Header />
   
   </>;
 }
---
+++ lexicon-task-react-radio/src/components/LissenDirectly.tsx
@@ -0,0 +1,16 @@
+import '../styles/Header.css';
+
+export const Header = () => {
+  return (
+    <header>
+      <ul>
+        <li>
+          <img src="https://t3.ftcdn.net/jpg/04/67/94/94/360_F_467949419_woOzXXQZNzl8Y6gfkRnefQlJkwP5dqur.jpg" alt="svt logo" />
+        </li>
+        <li>
+        <img src="https://t3.ftcdn.net/jpg/04/67/94/94/360_F_467949419_woOzXXQZNzl8Y6gfkRnefQlJkwP5dqur.jpg" alt="svt logo" />
+        </li>
+      </ul>
+    </header>
+  );
+}
