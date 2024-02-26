a3acad1c7976596c33195b15b8000cd8c81ff92e
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-26 18:45:13 +0100

    Refactor to take json instead as was possible to convert form xml

--- lexicon-task-react-radio/src/api/fetchData.ts
+++ lexicon-task-react-radio/src/api/fetchData.ts
@@ -5,11 +5,14 @@ export const fetchData = async (endpoint: string) => {
 
   try {
     const response = await fetch(`https://api.sr.se/api/v2/${endpoint}`);
-    const xmlData = await response.text();
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(xmlData, "text/xml");
-    data = xmlDoc;
-  } catch (err: unknown) { // had to add : unknown to get rid of error from eslint
+
+    if (!response.ok) {
+      error = `Error: ${response.status}`;
+      throw new Error(error);
+    }
+
+    data = await response.json();
+  } catch (err: unknown) {
     if (err instanceof Error) {
       error = err.message;
     } else {
@@ -21,6 +24,30 @@ export const fetchData = async (endpoint: string) => {
 
   return { data, isLoading, error };
 }
+
+// export const fetchData = async (endpoint: string) => {
+//   let data;
+//   let error;
+//   let isLoading = true;
+
+//   try {
+//     const response = await fetch(`https://api.sr.se/api/v2/${endpoint}`);
+//     const xmlData = await response.text();
+//     const parser = new DOMParser();
+//     const xmlDoc = parser.parseFromString(xmlData, "text/xml");
+//     data = xmlDoc;
+//   } catch (err: unknown) { // had to add : unknown to get rid of error from eslint
+//     if (err instanceof Error) {
+//       error = err.message;
+//     } else {
+//       error = 'An error occurred';
+//     }
+//   } finally {
+//     isLoading = false;
+//   }
+
+//   return { data, isLoading, error };
+// }
 
 // import xml2js from 'xml2js';
 
