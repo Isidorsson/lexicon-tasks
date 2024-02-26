3fe375fbe80ca2ef03b3f0e2ef39b11f6cf9c8b9
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-26 15:44:08 +0100

    Add Home component and update routing

--- lexicon-task-react-radio/src/App.tsx
+++ lexicon-task-react-radio/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import { Channel } from "./components/Channel";
 import { Header } from "./components/LissenDirectly";
+import { Home } from './components/Home';
 import { LoginForm } from "./components/LoginForm";
 import { MyPage } from "./components/MyPage";
 import { Navbar } from "./components/Navbar";
@@ -13,18 +14,11 @@ export function App() {
       <Navbar />
       <Header />
       <Routes>
-        <Route path="/Channel">
-          <Channel />
-        </Route>
-        <Route path="/Program">
-          <Program />
-        </Route>
-        <Route path="/MyPage">
-          <MyPage />
-        </Route>
-        <Route path="/Login">
-          <LoginForm />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/Channel" element={<Channel />} />
+        <Route path="/Program" element={<Program />} />
+        <Route path="/MyPage" element={<MyPage />} />
+        <Route path="/Login" element={<LoginForm />} />
       </Routes>
     </Router>
   );
---
+++ lexicon-task-react-radio/src/components/Home.tsx
@@ -0,0 +1,10 @@
+import '../styles/Home.css'
+
+export const Home = () => {
+  return (
+    <div className="home">
+      <h1>Hem</h1>
+      <p>Logga in för att se din sida</p>
+    </div>
+  )
+}
