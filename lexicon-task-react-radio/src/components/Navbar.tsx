d4f39ab162ca09d752273697ee4e8b10226c08eb
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-26 14:54:51 +0100

    Update Navbar component styling and logo image source

--- lexicon-task-react-radio/src/components/Navbar.tsx
+++ lexicon-task-react-radio/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import '../styles/Navbar.css';
 export function Navbar() {
   return (
     <nav className="navbar">
-      <img src="" alt="svt logo" />
+      <img src="https://t3.ftcdn.net/jpg/04/67/94/94/360_F_467949419_woOzXXQZNzl8Y6gfkRnefQlJkwP5dqur.jpg" alt="svt logo" />
       <input type="text" placeholder="Sök Kanal" />
       <ul>
         <li>Kanal</li>
