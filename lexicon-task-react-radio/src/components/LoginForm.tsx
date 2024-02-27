b6102f5d6f2c685a0d73eed3f6fecbbd1084b8e1
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-27 16:55:17 +0100

    Add email input field and show password checkbox to LoginForm component

--- lexicon-task-react-radio/src/components/LoginForm.tsx
+++ lexicon-task-react-radio/src/components/LoginForm.tsx
@@ -5,9 +5,18 @@ export const LoginForm = () => {
     <div className="login-form">
       <h1>Login</h1>
       <form>
-        <input type="text" autoComplete='' placeholder="Username" />
-        <input type="password" autoComplete='' placeholder="Password" />
+        <label htmlFor="email">Email:</label>
+        <input type="text" id="email" autoComplete='' placeholder="example@example.com" />
+
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" autoComplete='' placeholder="Password" />
+
         <button type="submit">Login</button>
+
+        <label htmlFor="show-password">Show Password:</label>
+        <input type="checkbox" id="show-password" />
+
+        <p>Don't have an account? <a href="/register">Create an account</a></p>
       </form>
     </div>
   )
